87f906fd43c1d466410c6dd4ac5b113ced4ed0f1
carterdcode <117289619+carterdcode@users.noreply.github.com>
2024-03-14 17:43:53 +0000

    Added presets and dropdown box to select them
    Saving is currently bugged with presets - only saves correctly if the preset is selected during that run, if you save without selecting the preset then preset will not be saved.
    
    Changed names of some of the buttons for clarity

--- preferences.py
+++ preferences.py
@@ -2,21 +2,43 @@ import tkinter as tk
 from tkinter import ttk
 import json
 
-# Create dictionary to store preferences
-preferences_states = {}
+#TODO - create class "PreferenceScreen" to contain all this shite
+# Then head to Main.py and create a simple button (can be shit for now) and have the button call upon the create_preferences_page in this class, need to get rid of create_main_page method (at the bottom) and instead rely on matts CodingScreen
+
+#Create dictionary to store preferences
+current_preferences_states = {}
+preset_preference_names = []
 
 def save_preferences():
-    # Get state of each checkbox and store in dictionary
-    preferences_states['Option 1'] = checkbox1_var.get()
-    preferences_states['Option 2'] = checkbox2_var.get()
-    preferences_states['Option 3'] = checkbox3_var.get()
-    preferences_states['Slider'] = slider_var.get()
+    #Get state of each checkbox and store in dictionary
+    #bottom 3 are currently placeholders as they have no selection boxes etc
 
-    # Save preferences to a JSON file
+    #Save preferences to a JSON file
+    current_preferences_states['show-line-numbers'] = checkbox1_var.get()
+    current_preferences_states['auto-save'] = checkbox2_var.get()
+    current_preferences_states['Option 3'] = checkbox3_var.get()
+    current_preferences_states['Slider'] = slider_var.get()
+    #TODO add rest of preferences here as they are added as dropdowns/options else they arent saved correctly when no preset is selected
     with open('preferences.json', 'w') as file:
-        json.dump(preferences_states, file)
+        json.dump(current_preferences_states, file)
 
-    print(preferences_states)
+    print(current_preferences_states)
+
+def update_current_preferences_to_selected_preset(selected_preset):
+    with open('preset-preferences.json', 'r') as file:
+        preset_preferences = json.load(file)
+        # update the current_preferences_states to the selected preset using the name of the preset as the key to get the values from the dictionary
+        for preset in preset_preferences:
+            if preset['name'] == selected_preset:
+                # Update the current_preferences_states to the selected preset
+                current_preferences_states.update(preset['settings'])
+                #add the rest of the custom options
+                current_preferences_states['show-line-numbers'] = checkbox1_var.get()
+                current_preferences_states['auto-save'] = checkbox2_var.get()
+                current_preferences_states['Option 3'] = checkbox3_var.get()
+                current_preferences_states['Slider'] = slider_var.get()
+                print(current_preferences_states)
+                break
 
 def update_slider_label(*args): # *args accept any number of arguments and ignore them cos we don't need them 
     #as we get the slider value from the slider_var global var
@@ -29,40 +51,57 @@ def create_preferences_page():
     # label for the slider which is updated in update_slider_label function above
 
     preferences_window = tk.Toplevel()  # Create a new top level window (aka a pop-up window seperete from the main window
-    # can change this to be part of main window if needed??)
+    #can change this to be part of main window if needed??)
     preferences_window.title("Preferences")
 
-    # Load preferences from preferences.json file and store in dictionary
+    #Load preferences from preferences.json file and store in dictionary
     with open('preferences.json', 'r') as file:
-        preferences_states = json.load(file)
+        current_preferences_states = json.load(file)
+
+    with open('preset-preferences.json', 'r') as file:
+        preset_preferences = json.load(file)
+        #get the names of the presets from the list of dictionaries and store them in a list
+        preset_preference_names = [preset['name'] for preset in preset_preferences]
 
     #Create frame to hold checkboxes
     checkboxes_frame = ttk.Frame(preferences_window, padding="20")
     checkboxes_frame.pack()
 
     #Create da checkboxes with boolean vars - true=ticked false=not ticked
-    checkbox1_var = tk.BooleanVar(value=preferences_states['Option 1'])
-    checkbox1 = ttk.Checkbutton(checkboxes_frame, text="Option 1", variable=checkbox1_var)
+    checkbox1_var = tk.BooleanVar(value=current_preferences_states['show-line-numbers'])
+    checkbox1 = ttk.Checkbutton(checkboxes_frame, text="Show line numbers", variable=checkbox1_var)
     checkbox1.pack()
 
-    checkbox2_var = tk.BooleanVar(value=preferences_states['Option 2'])
-    checkbox2 = ttk.Checkbutton(checkboxes_frame, text="Option 2", variable=checkbox2_var)
+    checkbox2_var = tk.BooleanVar(value=current_preferences_states['auto-save'])
+    checkbox2 = ttk.Checkbutton(checkboxes_frame, text="Auto-save", variable=checkbox2_var)
     checkbox2.pack()
 
-    checkbox3_var = tk.BooleanVar(value=preferences_states['Option 3'])
+    checkbox3_var = tk.BooleanVar(value=current_preferences_states['Option 3'])
     checkbox3 = ttk.Checkbutton(checkboxes_frame, text="Option 3", variable=checkbox3_var)
+
     checkbox3.pack()
+
+    def on_dropdown_selection(event):
+        selected_preset = dropdown.get()
+        print(selected_preset)
+        update_current_preferences_to_selected_preset(selected_preset)
+
+    dropdown = ttk.Combobox(checkboxes_frame, values=preset_preference_names)
+    dropdown.current(0)
+    dropdown.pack()
+
+    dropdown.bind("<<ComboboxSelected>>", on_dropdown_selection)
 
     #Create da frame to hold da slider
     slider_frame = ttk.Frame(preferences_window, padding="20")
     slider_frame.pack()
 
     #Create da label for da slider
-    slider_label = ttk.Label(slider_frame, text=f"Slider: {preferences_states['Slider']}")
+    slider_label = ttk.Label(slider_frame, text=f"Slider: {current_preferences_states['Slider']}")
     slider_label.pack(side="left")
 
     #Create da slider
-    slider_var = tk.IntVar(value=preferences_states['Slider'])
+    slider_var = tk.IntVar(value=current_preferences_states['Slider'])
     slider = ttk.Scale(slider_frame, from_=0, to=100, variable=slider_var, command=update_slider_label)
     slider.pack(side="left")
 
